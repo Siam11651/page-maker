7ff23f7845c072d4e49f856f78f48d7e8a2a99d5
Mustafa Siam Ur Rafique <siam11651@outlook.com>
2022-08-27 20:50:59 +0600

    done ig

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var quill = new Quill('#editor-container', {
     modules: {
       toolbar: [
-        [{ header: [1, 2, false] }],
+        [
+          {font: []},
+          {size: []},
+          {align: []},
+          {color: []}
+        ],
         ['bold', 'italic', 'underline'],
-        ['image', 'code-block']
+        [{'list': 'ordered'}, {'list': 'bullet'}],
+        ['image', 'formula']
       ]
     },
-    placeholder: 'Compose an epic...',
     theme: 'snow'  // or 'bubble'
   });
   
